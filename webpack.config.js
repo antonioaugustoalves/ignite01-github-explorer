496afd88ee7659b33f1fe742fa4e0910d3b890e1
Antonio A Alves <antonioaugusto.alves1981@gmail.com>
2021-10-19 13:28:43 -0300

    Finalizar configuração do webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
-const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+HtmlWebpackPlugin
 module.exports = {
+  mode:'development',
   entry:path.resolve(__dirname,'src','index.jsx'),
   output:{
     path:path.resolve(__dirname, 'dist'),
@@ -8,6 +11,11 @@ module.exports = {
   resolve:{
     extensions:['.js', '.jsx']
   },
+  plugins:[
+    new HtmlWebpackPlugin({
+      template:path.resolve(__dirname, 'public', 'index.html')
+    }),
+  ],
   module:{
     rules:[
       {
